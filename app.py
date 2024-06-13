0629e93d22006b624d950ac8b8bb40fe0fb8f105
Sulthan Daffa <94991158+DaffaDaff@users.noreply.github.com>
2024-06-13 17:28:19 +0700

    Add Camera Functionality and Loading Page

--- app.py
+++ app.py
@@ -21,7 +21,15 @@ LabelBase.register(name='Inter-Medium',
                    fn_regular='asset/font/Inter-Medium.ttf')
 
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.graphics import Line, Color, Rotate
+from kivy.graphics.texture import Texture
+import numpy as np
+import cv2
 
 class WindowManager(ScreenManager):
     pass
@@ -40,9 +48,12 @@ class LoginScreen(Screen):
 
         usr = app.config.getdefault(app.username, 'Username', ' ')
         pas = app.config.getdefault(app.username, 'Password', ' ')
+        self.ids['status'].text = ''
 
         if (usr == app.username and pas == app.password):
             self.manager.current = 'homeScreen'
+        else:
+            self.ids['status'].text = 'Username or Password Incorrect'
 
         app.config.write()
     
@@ -56,8 +67,12 @@ class SignupScreen(Screen):
 
         app.config.read(app.get_application_config('user.ini'))
 
+        self.ids['status'].text = ''
+
         if(not app.config.has_section(app.username)):
             self.manager.current = 'genderScreen'
+        else:
+            self.ids['status'].text = 'Username Already Taken'
 
 
         app.config.write()
@@ -78,9 +93,70 @@ class GenderScreen(Screen):
 class HomeScreen(Screen):
     username = StringProperty(None)
 
-    def on_enter(self):
+    def on_pre_enter(self):
         app = App.get_running_app()
         self.username = f'Welcome, {app.username}'
+
+class AgeScreen(Screen):
+    def start(self, age, weight):
+        app = App.get_running_app()
+
+        app.age = age
+        app.weight = weight
+
+        self.manager.current = 'cameraScreen'
+
+class CameraScreen(Screen):
+    img = Image()
+
+    def on_pre_enter(self):
+        self.cap = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0 / 30)
+
+    def update(self, dt):
+        ret, frame = self.cap.read()
+        if ret:
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            
+            self.img.texture = image_texture
+
+    def capture(self):
+        ret, frame = self.cap.read()
+        if ret:
+            App.get_running_app().img = frame
+            self.manager.current = 'loadingScreen'
+
+class SpinningLoader(Widget):
+    def __init__(self, start, end, col, **kwargs):
+        super().__init__(**kwargs)
+        self.angle_start = start
+        self.angle_end = end
+        with self.canvas:
+            self.color = Color(col[0], col[1], col[2], col[3])
+            self.line = Line(circle=(self.center_x, self.height, 20, self.angle_start, self.angle_end), width=3)
+
+        Clock.schedule_interval(self.update, 1 / 60.)
+
+    def update(self, dt):
+        self.angle_start += 6 % 360
+        self.angle_end += 6 % 360
+        self.line.circle = (self.center_x, self.height, 30, self.angle_start, self.angle_end)
+
+class LoadingScreen(Screen):
+    def on_pre_enter(self):
+        self.layout = self.ids['layout']
+        self.layout.add_widget(SpinningLoader(0, 10, (0.6, 0.6, 0.6, 1)))
+        self.layout.add_widget(SpinningLoader(72, 82, (0.6, 0.6, 0.6, 1)))
+        self.layout.add_widget(SpinningLoader(144, 154, (0.6, 0.6, 0.6, 1)))
+        self.layout.add_widget(SpinningLoader(216, 226, (0.6, 0.6, 0.6, 1)))
+        self.layout.add_widget(SpinningLoader(288, 298, (0.6, 0.6, 0.6, 1)))
+
+    def on_enter(self):
+        pass
 
 from kivy.lang import Builder
 kv = Builder.load_file('app.kv')
@@ -90,6 +166,9 @@ from kivy.app import App
 class CrealitsApp(App):
     username = StringProperty(None)
     password = StringProperty(None)
+    age = NumericProperty(0)
+    weight = NumericProperty(0)
+    img = np.zeros(2)
 
     def build(self):
         return kv
